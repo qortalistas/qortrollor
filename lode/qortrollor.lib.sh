f14534855fe0aa98ab3737a5dfe7da1bf1cfba4b
sermin asusi7 <sermin@asusi7.machine>
2023-08-28 01:27:26 +0200

    qortrollor command

--- lode/qortrollor.lib.sh
+++ lode/qortrollor.lib.sh
@@ -959,9 +959,9 @@ monitor_iteration() {
   api_get_higehst_peer_height() {
     local api_height peers
     api_height=$(curl -s "${QORTAL_API_BASE_URL}/admin/status" | jq -r '.height')
-    #    peers=$(curl -X GET "http://${QORTAL_API_BASE_URL}/peers" -H "accept: application/json")
-    peers='dummy'
-    info_line+="api_height: ${api_height}  peers: ${peers}"
+    peers=$(curl -X GET "${QORTAL_API_BASE_URL}/peers" -H "accept: application/json")
+    #    peers='dummy'
+    info_line+="  api_height: ${api_height}  peers: ${peers}"
     #    info_line=$(printf -v info_line 'api_height: "%s"  %s' "${api_height}" "${peers}")
 
     #    peers=$(curl -X GET "http://10.6.2.32:12391/peers" -H  "accept: application/json")
