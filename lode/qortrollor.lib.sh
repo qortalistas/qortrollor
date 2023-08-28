5fadddf607fd337d7e4603c37d49b00c44d32f57
sermin asusi7 <sermin@asusi7.machine>
2023-08-28 04:17:17 +0200

    qortrollor command

--- lode/qortrollor.lib.sh
+++ lode/qortrollor.lib.sh
@@ -1018,25 +1018,33 @@ monitor_iteration() {
     #    info_line+="  self: ${api_height}  high: ${peer_high}  diff: ${diff}  prog: ${height_progress}/${peer_high_progress}"
     #    printf -v info_line '%s self: %s  high: %s  diff: %s  prog: %s/%s'  "${info_line}" "${api_height}" "${peer_high}" "${diff}" "${height_progress}" "${peer_high_progress}"
 
-    #    height_progress_aligned="${height_progress}"
-    #    if [[ ${height_progress} -gt 0 ]]; then
-    #      height_progress_aligned="+${height_progress}"
-    #    elif [[ ${height_progress} == 0 ]]; then
-    #      height_progress=" ${height_progress}"
-    #    fi
-
     height_progress_aligned="${height_progress}"
     if [[ ${height_progress} -gt 0 ]]; then
-      height_progress_aligned="\033[32m+${height_progress}\033[0m" # Green for positive
-    elif [[ ${height_progress} -lt 0 ]]; then
-      height_progress_aligned="\033[31m${height_progress}\033[0m" # Red for negative
-#    else
-#      height_progress=" ${height_progress}"
+      height_progress_aligned="+${height_progress}"
+    elif [[ ${height_progress} == 0 ]]; then
+      height_progress=" ${height_progress}"
     fi
 
-    printf -v info_line '%s heights: %s/%s  prog: %s/%s  diff: %s' \
-      "${info_line}" "${api_height}" "${peer_high}" \
-      "${height_progress_aligned}" "${peer_high_progress}" "${diff}"
+    #    height_progress_aligned="${height_progress}"
+    #    if [[ ${height_progress} -gt 0 ]]; then
+    #      height_progress_aligned="\033[32m+${height_progress}\033[0m" # Green for positive
+    #    elif [[ ${height_progress} -lt 0 ]]; then
+    #      height_progress_aligned="\033[31m${height_progress}\033[0m" # Red for negative
+    #      #    else
+    #      #      height_progress=" ${height_progress}"
+    #    fi
+
+    #    #    formatter='%s heights: %s/%s  prog: %s/%s  diff: %s'
+    #    printf -v info_line '%s heights: %s/%s  prog: %s/%s  diff: %s' \
+    #      "${info_line}" "${api_height}" "${peer_high}" \
+    #      "${height_progress_aligned}" "${peer_high_progress}" "${diff}"
+
+    declare -a arguments=("${info_line}"
+      "${api_height}" "${peer_high}"
+      "${height_progress}" "${peer_high_progress}" "${diff}"
+    )
+    printf -v info_line '%s heights: %s/%s  prog: %s/%s  diff: %s' "${arguments[@]}"
+
   }
 
   api_get_higehst_peer_height
